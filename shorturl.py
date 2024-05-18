2ff50243945c29919f17cd335943981433d63776
Brandon Chastain <brandonchastain@gmail.com>
2024-05-17 23:38:31 -0700

    use turso

--- shorturl.py
+++ shorturl.py
@@ -1,13 +1,17 @@
 from bottle import route, run, request, response, redirect
 import base64
 import configparser
-import sqlite3
+from dotenv import load_dotenv
+import os
 
 # site name displayed in short links returned to users
-siteurl = ""
+site_uri = ""
 
 # sqllite file name
-dbname = "shorturls.db"
+db_name = "shorturls.db"
+db_uri = "libsql://genuine-magneto-bch13.turso.io"
+
+conn = None
 
 @route('/', method="get")
 def index():
@@ -15,7 +19,7 @@ def index():
         <h1>ShortUrl</h1>
         <form action="/" method="post">
             Long URL: <input name="longurl" type="text" /> <br/>
-            Short name (optional): <input name="name" type="text" />
+            Short name (optional): <input name="name" type="text" /> <br />
             <input value="Create" type="submit" />
         </form>
     '''
@@ -29,7 +33,6 @@ def post():
 
     if not (longurl.startswith("http://") or longurl.startswith("https://")):
         longurl = "https://" + longurl
-
     existingRec = getFromDb(name)
     if existingRec != None:
         response.status = 409
@@ -47,13 +50,14 @@ def post():
         <b>Short URL:</b>
         <a href="/%s">%s/%s</a>
         <button onclick="history.back()">Go Back</button>
-        ''' % (name, siteurl, name)
+        ''' % (name, site_uri, name)
 
 # Redirects the user to the longurl referred to by the provided shorturl.
 @route('/<name>', method="get")
 def get(name):
+    print(name)
     existingRec = getFromDb(name)
-
+    print(existingRec)
     if existingRec == None:
         response.status = 404
         return "404 Not Found"
@@ -63,39 +67,57 @@ def get(name):
 # Deletes a shorturl.
 @route('/<name>', method="delete")
 def delete(name):
-    # TODO: implement a lock or tx for get -> delete flow to avoid race conditions
     existingRec = getFromDb(name)
 
     if existingRec == None:
         response.status = 404
         return "404 Not Found"
     
-    with sqlite3.connect(dbname) as db:
-        db.execute("DELETE FROM urls WHERE shorturl = ?", [name])
+    conn = connect()
+    conn.execute("DELETE FROM urls WHERE shorturl = ?", tuple([name]))
+    conn.commit()
 
 # Access db data
 def getFromDb(shorturl):
-    with sqlite3.connect(dbname) as db:
-        res = db.execute("SELECT longurl from urls WHERE shorturl = ?", [shorturl])
-        longurl = res.fetchone()
-        return longurl[0] if longurl != None else None
+    print("connecting to get form db")
+    print("connected. executing...")
+    conn = connect()
+    row = conn.execute("SELECT longurl from urls WHERE shorturl = ?", tuple([shorturl])).fetchone()
+    if row == None:
+        conn.sync()
+        row = conn.execute("SELECT longurl from urls WHERE shorturl = ?", tuple([shorturl])).fetchone()
+    return row[0] if row != None else None
 
 # Add to data in db
 def addToDb(shorturl, longurl):
-    with sqlite3.connect(dbname) as db:
-        db.execute("INSERT INTO urls (shorturl, longurl) VALUES (?,?)", [shorturl, longurl])
+    conn = connect()
+    conn.execute("INSERT INTO urls (shorturl, longurl) VALUES (?,?)", tuple([shorturl, longurl]))
+    conn.commit()
+    conn.sync()
+
+def connect():
+    import libsql_experimental as libsql
+
+    tursotoken = os.environ.get("shorturl_tursotoken", "")
+    res = libsql.connect(db_name, sync_url=db_uri, auth_token=tursotoken)
+    print("result: %s" % (res))
+    return res
+
 
 def main():
+    load_dotenv()  # take environment variables from .env.
     config = configparser.ConfigParser()
     config.read('config.ini')
-    global siteurl
-    siteurl = config['shorturl']['siteurl']
+    global site_uri
+    site_uri = config['shorturl']['siteurl']
     port = config['shorturl']['port']
 
-    with sqlite3.connect(dbname) as db:
-        db.execute("CREATE TABLE IF NOT EXISTS urls(shorturl, longurl)")
+    global tursotoken
+    global conn
+    conn = connect()
+    conn.execute("CREATE TABLE IF NOT EXISTS urls(shorturl, longurl)")
+    conn.commit()
     
-    run(host='0.0.0.0', port=port, server="gunicorn")
+    run(host='0.0.0.0', port=port)
 
-if __name__ == "__main__":
-    main()
+main()
